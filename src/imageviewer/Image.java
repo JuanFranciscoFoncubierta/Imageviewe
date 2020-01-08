8123195dcefddc2284237b57071b8584e39b6cf0
juanf <juanf@DESKTOP-QC17FKA>
2020-01-08 18:06:39 +0100

    kata 1.2

---
+++ src/imageviewer/Image.java
@@ -0,0 +1,13 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imageviewer;
+
+public interface Image {
+    public String name();
+    public byte[] data();
+    public Image next();
+    public Image prev();
+}
---
+++ src/imageviewer/ImageDisplay.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package imageviewer;
+
+public interface ImageDisplay {
+    
+    public void show(Image image);
+
+    public Image getCurrentImage();
+    
+}
